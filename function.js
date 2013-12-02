8a33276c1a9927b4d820859d7fe96d3e945e9417
shariffh <shahuma2010@gmail.com>
2013-12-02 08:52:02 +0300

    first commit for data stracture

--- function.js
+++ function.js
@@ -35,3 +35,44 @@ print("inside test, the variable holds '" + variable + "'.");
 printVariable();
 }
 test();
+
+function makeAddFunction(amount) {
+function add(number) {
+return number + amount;
+}
+return add;
+}
+var addTwo = makeAddFunction(2);
+var addFive = makeAddFunction(5);
+show(addTwo(1) + addFive(1));
+
+function chicken() {
+return egg();
+}
+function egg() {
+return chicken();
+}
+print(chicken() + " came first.");
+
+/*a function findSequence*/
+function findSequence(goal) {
+function find(start, history) {
+if (start == goal)
+return history;
+else if (start > goal)
+return null;
+else
+return find(start + 5, "(" + history + " + 5)") ||
+find(start * 3, "(" + history + " * 3)");
+}
+return find(1, "1");
+}
+console.log(findSequence(24));
+
+function greaterThan(x) {
+return function(y) {
+return y > x;
+};
+}
+var greaterThanTen = greaterThan(10);
+console.log(greaterThanTen(9));
