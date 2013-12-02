7564dece0b5f96f2b4ce01a994ffe33eefaf6b86
shariffh <shahuma2010@gmail.com>
2013-12-02 08:07:34 +0300

    add comments to a function absolute

--- function.js
+++ function.js
@@ -11,10 +11,27 @@ return result;
 }
 show(power(2, 10));
 
+/*
+A function which returns the absolute value of the number it is given as its argument. 
+The absolute value of a negative number is the positive version of that same number, and the absolute
+ value of a positive number (or zero) is that number itself.
+ */
 function absolute(number) {
 if (number < 0)
 return -number;
 else
 return number;
 }
-console.log(absolute(-144));
+console.log(absolute(-36));
+
+var variable = "top-level";
+function printVariable() {
+print("inside printVariable, the variable holds '" +
+variable + "'.");
+}
+function test() {
+var variable = "local";
+print("inside test, the variable holds '" + variable + "'.");
+printVariable();
+}
+test();
