fbb9db59423e32acf1aa4785ec00e83ce2a8ef6e
shariffh <shahuma2010@gmail.com>
2013-12-02 07:24:41 +0300

    test exercuse the privious exercises using for loop

--- script.js
+++ script.js
@@ -24,3 +24,7 @@ line = line + "#";
 console.log(line);
 counter = counter + 1;
 }
+var result = 1;
+for (var counter = 0; counter < 10; counter = counter + 1)
+result = result * 2;
+console.log(result);
