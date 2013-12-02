36cf3aa0cba8f556c5340f939d3bb905cdda899f
shariffh <shahuma2010@gmail.com>
2013-12-02 11:04:10 +0300

    add hasSevenTruths() function

--- error-handling.js
+++ error-handling.js
@@ -30,3 +30,30 @@ finally {
 currentThing = null;
 }
 }
+
+
+var FoundSeven = {};
+function hasSevenTruths(object) {
+var counted = 0;
+function count(object) {
+for (var name in object) {
+if (object[name] === true) {
+counted++;
+if (counted == 7)
+throw FoundSeven;
+}
+else if (typeof object[name] == "object") {
+count(object[name]);
+}
+}
+}
+try {
+count(object);
+return false;
+}
+catch (exception) {
+if (exception != FoundSeven)
+throw exception;
+return true;
+}
+}
