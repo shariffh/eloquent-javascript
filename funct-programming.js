80e9994a9ee8bdbae05755d31d3a83fe74e47135
shariffh <shahuma2010@gmail.com>
2013-12-02 12:03:11 +0300

    add codes for exercise 6.3

--- funct-programming.js
+++ funct-programming.js
@@ -49,3 +49,42 @@ content: paragraph};
 }
 console.log(processParagraph(paragraphs[0]));
 var paragraphs = map(processParagraph, recluseFile().split("\n\n"));
+
+/*
+Build a function splitParagraph which, given a paragraph string, returns an array of paragraph fragments. 
+Think of a good way to represent the fragments.
+*/
+function splitParagraph(text) {
+function indexOrEnd(character) {
+var index = text.indexOf(character);
+return index == -1 ? text.length : index;
+}
+function takeNormal() {
+var end = reduce(Math.min, text.length,
+map(indexOrEnd, ["*", "{"]));
+var part = text.slice(0, end);
+text = text.slice(end);
+return part;
+}
+function takeUpTo(character) {
+var end = text.indexOf(character, 1);
+if (end == -1)
+throw new Error("Missing closing '" + character + "'");
+var part = text.slice(1, end);
+text = text.slice(end + 1);
+return part;
+}
+var fragments = [];
+while (text != "") {
+if (text.charAt(0) == "*")
+fragments.push({type: "emphasised",
+content: takeUpTo("*")});
+else if (text.charAt(0) == "{")
+fragments.push({type: "footnote",
+content: takeUpTo("}")});
+else
+fragments.push({type: "normal",
+content: takeNormal()});
+}
+return fragments;
+}
