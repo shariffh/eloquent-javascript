6739f869429d788bf54f3b360f4dee60eb4c15b2
shariffh <shahuma2010@gmail.com>
2013-12-02 12:06:20 +0300

    add extractFootnotes() function

--- funct-programming.js
+++ funct-programming.js
@@ -88,3 +88,24 @@ content: takeNormal()});
 }
 return fragments;
 }
+
+function extractFootnotes(paragraphs) {
+var footnotes = [];
+var currentNote = 0;
+function replaceFootnote(fragment) {
+if (fragment.type == "footnote") {
+currentNote++;
+footnotes.push(fragment);
+fragment.number = currentNote;
+return {type: "reference", number: currentNote};
+}
+else {
+return fragment;
+}
+}
+forEach(paragraphs, function(paragraph) {
+paragraph.content = map(replaceFootnote,
+paragraph.content);
+});
+return footnotes;
+}
