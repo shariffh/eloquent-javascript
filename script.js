9f29885a9541ce33f3b656515596db1ddbc9d8f8
shariffh <shahuma2010@gmail.com>
2013-12-02 07:25:26 +0300

    test exercuse the privious exercises using for loop

--- script.js
+++ script.js
@@ -28,3 +28,8 @@ var result = 1;
 for (var counter = 0; counter < 10; counter = counter + 1)
 result = result * 2;
 console.log(result);
+var line = "";
+for (var counter = 0; counter < 10; counter = counter + 1) {
+line = line + "#";
+console.log(line);
+}
