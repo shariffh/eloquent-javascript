1a1a8211104433513ef983fac7ce220a30dff3fe
shariffh <shahuma2010@gmail.com>
2013-12-02 07:15:54 +0300

    test exercuse 2.2

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ while (count <= 10) {
 console.log(total);
 ((4 >= 6) || ("grass" != "green")) &&
 !(((12 * 2) == 144) && true)
+var D = new Date(); var hours = D.getHours(); 
+var minutes = D.getMinutes(); var seconds = D.getSeconds(); 
+var time = hours + ":" + minutes + ":" + seconds; 
+document.write("The time is now " + time);
+var result = 1;
+var counter = 0;
+while (counter < 10) {
+result = result * 2;
+counter = counter + 1;
+}
+console.log(result);
