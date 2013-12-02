680aa653733fee5c85170163e3cac6b8d41f0bc6
shariffh <shahuma2010@gmail.com>
2013-12-02 08:01:42 +0300

    add codes for add and power function

---
+++ function.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+return a + b;
+}
+console.log(add(2, 2));
+
+function power(base, exponent) {
+var result = 1;
+for (var count = 0; count < exponent; count++)
+result *= base;
+return result;
+}
+show(power(2, 10));
