d603f83e44f3a0c2675bbb315d6a59e0df5f7d9e
shariffh <shahuma2010@gmail.com>
2013-12-03 14:24:41 +0300

    use call method

--- chapter8.js
+++ chapter8.js
@@ -11,3 +11,4 @@ var fatRabbit = {adjective: "fat", speak: speak};
 whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
 fatRabbit.speak("I could sure use a carrot right now.");
 speak.apply(fatRabbit, ["Yum."]);
+speak.call(fatRabbit, "Burp.");
