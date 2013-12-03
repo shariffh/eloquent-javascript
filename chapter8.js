e59075472f7f7d8ae2f2c0e2448176fd5e173b8e
shariffh <shahuma2010@gmail.com>
2013-12-03 14:22:22 +0300

    call a function as a method

--- chapter8.js
+++ chapter8.js
@@ -3,3 +3,10 @@ rabbit.speak = function(line) {
 console.log("The rabbit says '", line, "'");
 };
 rabbit.speak("Well, now you're asking me.");
+function speak(line) {
+console.log("The ", this.adjective, " rabbit says '", line, "'");
+}
+var whiteRabbit = {adjective: "white", speak: speak};
+var fatRabbit = {adjective: "fat", speak: speak};
+whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
+fatRabbit.speak("I could sure use a carrot right now.");
