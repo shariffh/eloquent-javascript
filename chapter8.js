53968d81b4aec7fa660ac6dd66cce13f8f32aa0e
shariffh <shahuma2010@gmail.com>
2013-12-03 14:23:55 +0300

    use of apply method

--- chapter8.js
+++ chapter8.js
@@ -10,3 +10,4 @@ var whiteRabbit = {adjective: "white", speak: speak};
 var fatRabbit = {adjective: "fat", speak: speak};
 whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
 fatRabbit.speak("I could sure use a carrot right now.");
+speak.apply(fatRabbit, ["Yum."]);
