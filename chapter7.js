cdb8a73edd7ed09af3c52104b0737c823cbf46bc
shariffh <shahuma2010@gmail.com>
2013-12-03 08:29:21 +0300

    add a function that returns an array of route objects

--- chapter7.js
+++ chapter7.js
@@ -126,4 +126,25 @@ result.push(element);
 });
 return result;
 }
-show(filter(partial(op[">"], 5), [0, 4, 8, 12]));
+console.log(filter(partial(op[">"], 5), [0, 4, 8, 12]));
+
+function possibleRoutes(from, to) {
+function findRoutes(route) {
+function notVisited(road) {
+return !member(route.places, road.to);
+}
+function continueRoute(road) {
+return findRoutes({places: route.places.concat([road.to]),
+length: route.length + road.distance});
+}
+var end = route.places[route.places.length - 1];
+if (end == to)
+return [route];
+else
+return flatten(map(continueRoute, filter(notVisited,
+roadsFrom(end))));
+}
+return findRoutes({places: [from], length: 0});
+}
+console.log(possibleRoutes("Point Teohotepapapa", "Point Kiukiu").length);
+console.log(possibleRoutes("Hanapaoa", "Mt Ootua"));
