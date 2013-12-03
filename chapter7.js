83edb9fb6cd99171f92c5007f867f988ef1d2c7d
shariffh <shahuma2010@gmail.com>
2013-12-03 07:52:21 +0300

    make a function makeRoads

--- chapter7.js
+++ chapter7.js
@@ -7,12 +7,22 @@ var roads = {
               {to: "Point Kiukiu", distace: 15}
           /* et cetera */]};
 var roads = {};
-function makeRoad(from, to, length) {
-function addRoad(from, to) {
-if (!(from in roads))
-roads[from] = [];
-roads[from].push({to: to, distance: length});
-}
-addRoad(from, to);
-addRoad(to, from);
-}
+    function makeRoad(from, to, length) {
+    function addRoad(from, to) {
+     if (!(from in roads))
+      roads[from] = [];
+      roads[from].push({to: to, distance: length});
+   }
+     addRoad(from, to);
+     addRoad(to, from);
+   }
+
+   /*Ex. 7.1
+   Write a function makeRoads that takes any uneven number of arguments. 
+   The first argument is always the starting point of the roads, and every pair of 
+   arguments after that gives an ending point and a distance.
+   */
+   function makeRoads(start) {
+      for (var i = 1; i < arguments.length; i += 2)
+        makeRoad(start, arguments[i], arguments[i + 1]);
+   }
