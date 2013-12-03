8473d3ae29d2d6b5c9b41b42bcc9632427d12ce0
shariffh <shahuma2010@gmail.com>
2013-12-03 15:09:05 +0300

    give all objects a method called properties which returns an array with the names of non-hidden propertie

--- chapter8.js
+++ chapter8.js
@@ -54,3 +54,14 @@ console.log("The ", this.adjective, " rabbit says '", line, "'");
 };
 var hazelRabbit = new Rabbit("hazel");
 hazelRabbit.speak("Good Frith!");
+var noCatsAtAll = {};
+if ("constructor" in noCatsAtAll)
+console.log("Yes, there definitely is a cat called 'constructor'.");
+Object.prototype.properties = function() {
+var result = [];
+for (var property in this)
+result.push(property);
+return result;
+};
+var test = {x: 10, y: 3};
+console.log(test.properties());
