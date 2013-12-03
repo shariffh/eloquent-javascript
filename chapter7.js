21bf36d5c03e4bd9381df127b895641bb49d1e69
shariffh <shahuma2010@gmail.com>
2013-12-03 07:58:36 +0300

    add a function roadFrom()

--- chapter7.js
+++ chapter7.js
@@ -26,3 +26,22 @@ var roads = {};
       for (var i = 1; i < arguments.length; i += 2)
         makeRoad(start, arguments[i], arguments[i + 1]);
    }
+
+   var roads = {};
+makeRoads("Point Kiukiu", "Hanaiapa", 19,"Mt Feani", 15, "Taaoa", 15);
+makeRoads("Airport", "Hanaiapa", 6, "Mt Feani", 5,"Atuona", 4, "Mt Ootua", 11);
+makeRoads("Mt Temetiu", "Mt Feani", 8, "Taaoa", 4);
+makeRoads("Atuona", "Taaoa", 3, "Hanakee pearl lodge", 1);
+makeRoads("Cemetery", "Hanakee pearl lodge", 6, "Mt Ootua", 5);
+makeRoads("Hanapaoa", "Mt Ootua", 3);
+makeRoads("Puamua", "Mt Ootua", 13, "Point Teohotepapapa", 14);
+show(roads["Airport"]);
+
+function roadsFrom(place) {
+    var found = roads[place];
+     if (found == undefined)
+      throw new Error("No place named '" + place + "' found.");
+      else
+        return found;
+   }
+    show(roadsFrom("Puamua"));
