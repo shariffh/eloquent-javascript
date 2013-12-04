cca5b3e9ba3408dd4ccc2dd6e1601f0b42ca7d0b
shariffh <shahuma2010@gmail.com>
2013-12-04 15:50:06 +0300

    reate new file info.html

--- chapter11.js
+++ chapter11.js
@@ -1,1 +1,43 @@
 var perry = window.open("http://www.pbfcomics.com");
+
+show(perry.Math);
+
+perry.close();
+console.log(document.location.href);
+
+console.log(timeWriter);
+var time = viewHTML(timeWriter);
+
+time.close();
+
+var encoded = encodeURIComponent("aztec empire");
+console.log(encoded);
+console.log(decodeURIComponent(encoded));
+var form = window.open("info.html");
+
+attach(form);
+console.log(document.location.href);
+console.log(document.title);
+show(form);
+
+var userForm = document.forms.userinfo;
+console.log(userForm.method);
+console.log(userForm.action);
+
+var nameField = userForm.elements.name;
+nameField.value = "EugÎáÎéÎýne";
+
+function validInfo(form) {
+  return form.elements.name.value != "" &&
+      /^.+@.+\.\w{2,3}$/.test(form.elements.email.value);
+      }
+
+      console.log(validInfo(document.forms.userinfo));
+
+userForm.elements.send.onclick = function() {
+if (validInfo(userForm))
+    userForm.submit();
+      else
+          console.log("Give us a name and a valid e-mail address!");
+
+  };
