409ad764ae97652b2c3a2c2436226e99064c5cd2
shariffh <shahuma2010@gmail.com>
2013-12-04 07:14:32 +0300

    add a function dictionary

--- chapter8.js
+++ chapter8.js
@@ -98,3 +98,27 @@ console.log("Property ", name, " = ", value);
 var object = {foo: "bar"};
 console.log(Object.prototype.hasOwnProperty.call(object, "foo") && 
 	        Object.prototype.propertyIsEnumerable.call(object, "foo"));
+function Dictionary(startValues) {
+this.values = startValues || {};
+}
+Dictionary.prototype.store = function(name, value) {
+this.values[name] = value;
+};
+Dictionary.prototype.lookup = function(name) {
+return this.values[name];
+};
+Dictionary.prototype.contains = function(name) {
+return Object.prototype.hasOwnProperty.call(this.values, name) &&
+Object.prototype.propertyIsEnumerable.call(this.values, name);
+};
+Dictionary.prototype.each = function(action) {
+forEachIn(this.values, action);
+};
+var colours = new Dictionary({Grover: "blue",
+Elmo: "orange",
+Bert: "yellow"});
+console.log(colours.contains("Grover"));
+console.log(colours.contains("constructor"));
+colours.each(function(name, colour) {
+console.log(name, " is ", colour);
+});
