1ff9d9b88189a273b83854d62582359eb6662673
shariffh <shahuma2010@gmail.com>
2013-12-04 09:23:13 +0300

    use the each method on the directions dictionary

--- chapter8.js
+++ chapter8.js
@@ -250,3 +250,16 @@ found.push({object: value, point: point});
 });
 return found;
 };
+
+Terrarium.prototype.listSurroundings = function(center) {
+var result = {};
+var grid = this.grid;
+directions.each(function(name, direction) {
+var place = center.add(direction);
+if (grid.isInside(place))
+result[name] = characterFromElement(grid.valueAt(place));
+else
+result[name] = "#";
+});
+return result;
+};
