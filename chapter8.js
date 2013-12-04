cf149e7f8d9d005d7fee7d5a9d756d0e0496d5c6
shariffh <shahuma2010@gmail.com>
2013-12-04 07:26:06 +0300

    use array of arrays to store the value on the grid

--- chapter8.js
+++ chapter8.js
@@ -134,3 +134,6 @@ Point.prototype.isEqualTo = function(other) {
 return this.x == other.x && this.y == other.y;
 };
 console.log((new Point(3, 1)).add(new Point(2, 4)));
+var grid = [["0,0", "1,0", "2,0"],
+["0,1", "1,1", "2,1"]];
+console.log(grid[1][2]);
