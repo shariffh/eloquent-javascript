a7f98672f69e59373d36b8ba8afed021de985762
shariffh <shahuma2010@gmail.com>
2013-12-04 07:28:09 +0300

    use a single arrays to store the value on the grid

--- chapter8.js
+++ chapter8.js
@@ -137,3 +137,6 @@ console.log((new Point(3, 1)).add(new Point(2, 4)));
 var grid = [["0,0", "1,0", "2,0"],
 ["0,1", "1,1", "2,1"]];
 console.log(grid[1][2]);
+var grid = ["0,0", "1,0", "2,0",
+"0,1", "1,1", "2,1"];
+console.log(grid[2 + 1 * 3]);
