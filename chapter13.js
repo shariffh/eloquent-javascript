b7950095b9fefe5af48eeda782ad924d0c89650e
shariffh <shahuma2010@gmail.com>
2013-12-06 06:57:51 +0300

    add a function registerEventHandler

--- chapter13.js
+++ chapter13.js
@@ -1,2 +1,11 @@
-$("button").addEventListener("click", function(){print("Click!");},
+$("button").addEventListener("click", function(){console.log("Click!");},
 false);
+
+function registerEventHandler(node, event, handler) {
+if (typeof node.addEventListener == "function")
+node.addEventListener(event, handler, false);
+else
+node.attachEvent("on" + event, handler);
+}
+registerEventHandler($("button"), "click",
+function(){console.log("Click (2)");});
