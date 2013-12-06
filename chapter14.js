5c84a686bfb01f897affd759a96bf6c6c1672d2c
shariffh <shahuma2010@gmail.com>
2013-12-06 19:32:14 +0300

    use open method to configure a request

--- chapter14.js
+++ chapter14.js
@@ -7,4 +7,10 @@ try {return new ActiveXObject("Microsoft.XMLHTTP");}
 catch (error) {}
 throw new Error("Could not create HTTP request object.");
 }
-show(typeof(makeHttpObject()));
+console.log(typeof(makeHttpObject()));
+
+
+var request = makeHttpObject();
+request.open("GET", "files/fruit.txt", false);
+request.send(null);
+print(request.responseText);
