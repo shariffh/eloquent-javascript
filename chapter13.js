20f92ed926196966751c614a3b3b433907f301cb
shariffh <shahuma2010@gmail.com>
2013-12-06 07:00:46 +0300

    define registerEventHandler to hold a different function

--- chapter13.js
+++ chapter13.js
@@ -9,3 +9,12 @@ node.attachEvent("on" + event, handler);
 }
 registerEventHandler($("button"), "click",
 function(){console.log("Click (2)");});
+
+if (typeof document.addEventListener == "function")
+var registerEventHandler = function(node, event, handler) {
+node.addEventListener(event, handler, false);
+};
+else
+var registerEventHandler = function(node, event, handler) {
+node.attachEvent("on" + event, handler);
+};
